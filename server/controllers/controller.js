491c08a5571a919fbaa80a531dbbe4b04d3513f3
Kartik <kvempati11@gmail.com>
2016-01-13 10:22:11 -0800

    Get response from forecast.io api

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -132,9 +132,9 @@ module.exports = {
   // Gets the weather report for the location specified 
   // at the time specified 
   fetchWeatherData: function (req, res, next) {
-    request('https://api.forecast.io/forecast/c1886414ac678c908104c2a20e4874c5/37.783583,-122.409063,2016-01-12T12:00:00', function (err, response, body) {
+    request('https://api.forecast.io/forecast/c1886414ac678c908104c2a20e4874c5/37.783583,-122.409063,2016-01-13T12:00:00', function (err, response, body) {
       if (!err) {
-        res.send(body);
+        
       }
     })
   },
